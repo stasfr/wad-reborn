f80eed16ba8a3c5a75a5f651c13a4d2cc5559e6a
stas_fr <stas240600@yandex.ru>
2024-09-14 16:25:25 +0300

    reinstall and update tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from "daisyui";
+
 export default {
-  content: [],
+  darkMode: "class",
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      xs: "480px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+    },
     extend: {},
   },
-  plugins: [],
-}
-
+  plugins: [daisyui],
+};
