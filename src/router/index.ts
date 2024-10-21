77d7550043c45556e6095b8e60004d9e59015388
stasfr <sfworking@yandex.ru>
2024-10-21 14:22:14 +0300

    docs: add todos

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,6 +6,10 @@ import {
 } from 'vue-router'
 import { routes } from 'vue-router/auto-routes'
 
+// TODO: requiresAuth для:
+// /user/profile
+// /grapes/notes and etc.
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
@@ -15,7 +19,7 @@ async function redirectToLogin(next: NavigationGuardNext) {
   const userStore = useUser()
   const userSession = await userStore.getSession()
   if (userSession === null) {
-    next('/login')
+    next('/user/login')
   } else {
     next()
   }
@@ -25,7 +29,7 @@ async function redirectToProfile(next: NavigationGuardNext) {
   const userStore = useUser()
   const userSession = await userStore.getSession()
   if (userSession !== null) {
-    next('/profile')
+    next('/user/profile')
   } else {
     next()
   }
@@ -34,7 +38,7 @@ async function redirectToProfile(next: NavigationGuardNext) {
 router.beforeEach((to, from, next: NavigationGuardNext) => {
   if (to.meta.requiresAuth) {
     redirectToLogin(next)
-  } else if (to.path === '/login') {
+  } else if (to.path === '/user/login') {
     redirectToProfile(next)
   } else {
     next()
