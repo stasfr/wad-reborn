4619be183daed036f9570ae91eab74ad2121d410
stasfr <sfworking@yandex.ru>
2024-10-21 13:59:32 +0300

    feat: migrate register form to primevue

--- src/components/Forms/Auth/Register.vue
+++ src/components/Forms/Auth/Register.vue
@@ -1,48 +1,90 @@
 <script setup lang="ts">
-const email = ref('stas240600@yandex.ru')
-const password = ref('password')
-const passwordRepeat = ref('passwordd')
+import { Icon } from '@iconify/vue'
+
+const registerData = ref({
+  email: 'stas240600@yandex.ru',
+  password: 'password',
+  passwordRepeat: 'password',
+})
+
 const router = useRouter()
 
 const userStore = useUser()
 
 async function signUp() {
-  if (password.value === passwordRepeat.value) {
+  // TODO: более комплексная валидация
+  if (registerData.value.password === registerData.value.passwordRepeat) {
     await userStore.signUp({
-      email: email.value,
-      password: password.value,
+      email: registerData.value.email,
+      password: registerData.value.password,
     })
-    router.push({ path: '/login' })
+    router.push({ path: '/user/login' })
   } else {
-    //TODO: throw modal with error
+    //TODO: throw toast
     console.log('Пароли не совпадают')
+  }
+}
+
+function resetForm() {
+  registerData.value = {
+    email: '',
+    password: '',
+    passwordRepeat: '',
   }
 }
 </script>
 
 <template>
-  <form class="flex flex-col space-y-4" @submit.prevent="">
-    <h2 class="text-center">Регистрация</h2>
+  <Card>
+    <template #title>
+      <div class="flex justify-between mb-4 items-center">
+        <h2>Регистрация</h2>
+        <Button @click="resetForm" severity="secondary" link>
+          <template #icon>
+            <Icon class="size-5" icon="ri:reset-left-line" />
+          </template>
+        </Button>
+      </div>
+    </template>
 
-    <AuthInput
-      type="email"
-      placeholder="email"
-      v-model="email"
-      icon="prime:user"
-    />
-    <AuthInput
-      type="password"
-      placeholder="пароль"
-      v-model="password"
-      icon="prime:key"
-    />
-    <AuthInput
-      type="password"
-      placeholder="пароль повторно"
-      v-model="passwordRepeat"
-      icon="prime:key"
-    />
+    <template #content>
+      <div class="flex flex-col gap-4 mb-4">
+        <FloatLabel variant="on">
+          <InputText
+            id="email"
+            type="text"
+            v-model="registerData.email"
+            class="w-full"
+          />
+          <label for="email">Email</label>
+        </FloatLabel>
 
-    <button class="btn" @click="signUp">Регистрация</button>
-  </form>
+        <FloatLabel variant="on">
+          <Password
+            v-model="registerData.password"
+            inputId="password"
+            :feedback="false"
+            toggleMask
+          />
+          <label for="password">Пароль</label>
+        </FloatLabel>
+
+        <FloatLabel variant="on">
+          <Password
+            v-model="registerData.passwordRepeat"
+            inputId="passwordRepeat"
+            :feedback="false"
+            toggleMask
+          />
+          <label for="passwordRepeat">Пароль повторно</label>
+        </FloatLabel>
+      </div>
+    </template>
+
+    <template #footer>
+      <div class="flex justify-end">
+        <Button class="" label="Регистрация" @click="signUp" />
+      </div>
+    </template>
+  </Card>
 </template>
